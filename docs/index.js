624a89b24ef8b345a664714f5e3b4f92579f2645
MarkJhem <markjhemamerna@gmail.com>
2024-11-12 23:03:14 +0800

    modified codes

--- docs/index.js
+++ docs/index.js
@@ -56,3 +56,28 @@ window.addEventListener('DOMContentLoaded', ()=> {
         }, 2300)
     })
 })
+
+
+
+// let tl = gsap.timeline({scrollTrigger: {
+//     trigger: "#main",
+//     markers: true,
+//     start: "50% 50%",
+//     end: "100% 50%",
+//     scrub:2,
+//     pin: true
+// }})
+
+// tl
+// .to("#top", {
+//     top: "-50%"
+// }, 'a')
+// .to("#bottom", {
+//     bottom: "-50%"
+// }, 'a')
+// .to("#top-h", {
+//     top: "80%"
+// }, 'a')
+// .to("#bottom-h", {
+//     bottom: "-80%"
+// }, 'a')
