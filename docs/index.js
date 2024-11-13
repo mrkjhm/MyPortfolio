7e8bcfc21c0f3ee97c370116a21033f9aa90d1f6
MarkJhem <markjhemamerna@gmail.com>
2024-11-13 21:11:51 +0800

    modified codes

--- docs/index.js
+++ docs/index.js
@@ -59,25 +59,25 @@ window.addEventListener('DOMContentLoaded', ()=> {
 
 
 
-// let tl = gsap.timeline({scrollTrigger: {
-//     trigger: "#main",
-//     markers: true,
-//     start: "50% 50%",
-//     end: "100% 50%",
-//     scrub:2,
-//     pin: true
-// }})
+let tl = gsap.timeline({scrollTrigger: {
+    trigger: "#main",
+    // markers: true,
+    // start: "50% 50%",
+    // end: "100% 50%",
+    scrub:2,
+    pin: true
+}})
 
-// tl
-// .to("#top", {
-//     top: "-50%"
-// }, 'a')
-// .to("#bottom", {
-//     bottom: "-50%"
-// }, 'a')
-// .to("#top-h", {
-//     top: "80%"
-// }, 'a')
-// .to("#bottom-h", {
-//     bottom: "-80%"
-// }, 'a')
+tl
+.to("#top", {
+    top: "-50%"
+}, 'a')
+.to("#bottom", {
+    bottom: "-50%"
+}, 'a')
+.to("#top-h", {
+    top: "80%"
+}, 'a')
+.to("#bottom-h", {
+    bottom: "-80%"
+}, 'a')
